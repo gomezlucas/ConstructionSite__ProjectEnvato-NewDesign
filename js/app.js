578a586a29889b53fae692ac26bf2731f053b092
Lucas <gomez-lucas@live.com.ar>
2020-07-01 02:03:31 -0300

    animations added

--- js/app.js
+++ js/app.js
@@ -40,6 +40,14 @@
         return false;
     });
 
+    //initiliaze AOS
+    AOS.init(
+        { offset: 150, // offset (in px) from the original trigger point
+             // values from 0 to 3000, with step 50ms
+
+        }
+    );
+ 
 
 })(jQuery); // End of use strict
 
